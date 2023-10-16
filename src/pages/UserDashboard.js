d9bf2b8b3f1392fa946bb7ba620dafcf13c61f27
krishna <kvbhashinge@gmail.com>
2023-10-16 18:26:18 +0530

    project added

---
+++ src/pages/UserDashboard.js
@@ -0,0 +1,318 @@
+import "./UserDashboard.css";
+
+const UserDashboard = () => {
+  return (
+    <div className="user-dashboard">
+      <div className="frame">
+        <div className="overview">
+          <img className="overview-child" alt="" src="/rectangle-16.svg" />
+          <div className="chevron-down-1-parent">
+            <img
+              className="chevron-down-1-icon"
+              alt=""
+              src="/chevrondown-1.svg"
+            />
+            <div className="quarterly">Quarterly</div>
+          </div>
+          <div className="overview1">Overview</div>
+          <div className="monthly-earning">Monthly Earning</div>
+          <div className="jan">Jan</div>
+          <div className="feb">Feb</div>
+          <div className="mar">Mar</div>
+          <div className="apr">Apr</div>
+          <div className="may">May</div>
+          <div className="jun">Jun</div>
+          <div className="jul">Jul</div>
+          <b className="aug">Aug</b>
+          <div className="sep">Sep</div>
+          <div className="oct">Oct</div>
+          <div className="nov">Nov</div>
+          <div className="dec">Dec</div>
+          <div className="overview-item" />
+          <div className="overview-inner" />
+          <div className="rectangle-div" />
+          <div className="overview-child1" />
+          <div className="overview-child2" />
+          <div className="overview-child3" />
+          <div className="overview-child4" />
+          <div className="overview-child5" />
+          <div className="overview-child6" />
+          <div className="overview-child7" />
+          <div className="overview-child8" />
+          <div className="overview-child9" />
+        </div>
+        <div className="customers">
+          <img className="customers-child" alt="" src="/rectangle-29.svg" />
+          <div className="customers1">Customers</div>
+          <div className="customers-that-buy">Customers that buy products</div>
+          <div className="customers-item" />
+          <div className="customers-inner" />
+          <div className="ellipse-div" />
+          <div className="customers-child1" />
+          <div className="total-new-customers-container">
+            <p className="p">
+              <b>65%</b>
+            </p>
+            <p className="total-new-customers">Total New Customers</p>
+          </div>
+        </div>
+        <div className="product">
+          <img className="product-child" alt="" src="/rectangle-30.svg" />
+          <div className="product-sell">Product Sell</div>
+          <div className="in-stock-parent">
+            <div className="in-stock">32 in stock</div>
+            <div className="div">$ 45.99</div>
+            <div className="div1">20</div>
+            <div className="abstract-3d-parent">
+              <div className="abstract-3d">Abstract 3D</div>
+              <div className="lorem-ipsum-dolor">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </div>
+              <img
+                className="unsplashwh2fhl0v-eo-icon"
+                alt=""
+                src="/unsplashwh2fhl0v-eo@2x.png"
+              />
+            </div>
+          </div>
+          <div className="sarphens-illustration-parent">
+            <div className="sarphens-illustration">Sarphens Illustration</div>
+            <div className="in-stock1">32 in stock</div>
+            <div className="div2">$ 45.99</div>
+            <div className="div3">20</div>
+            <div className="lorem-ipsum-dolor">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+            <img
+              className="unsplashwh2fhl0v-eo-icon"
+              alt=""
+              src="/unsplasha2j-namzag@2x.png"
+            />
+          </div>
+          <div className="landing-page-3d-max-parent">
+            <div className="sarphens-illustration">Landing Page 3D max</div>
+            <div className="in-stock1">32 in stock</div>
+            <div className="div2">$ 45.99</div>
+            <div className="div3">20</div>
+            <div className="lorem-ipsum-dolor">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+            <img
+              className="unsplashwh2fhl0v-eo-icon"
+              alt=""
+              src="/unsplashrazu-r66vuc@2x.png"
+            />
+          </div>
+          <div className="collab-illustration-parent">
+            <div className="sarphens-illustration">Collab Illustration</div>
+            <div className="in-stock1">32 in stock</div>
+            <div className="div2">$ 45.99</div>
+            <div className="div3">20</div>
+            <div className="lorem-ipsum-dolor">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+            <img
+              className="unsplashwh2fhl0v-eo-icon"
+              alt=""
+              src="/unsplashcpccybprfa@2x.png"
+            />
+          </div>
+          <div className="frame-parent">
+            <div className="product-name-parent">
+              <div className="product-name">Product Name</div>
+              <div className="stock">Stock</div>
+              <div className="price">Price</div>
+              <div className="total-sales">Total Sales</div>
+            </div>
+            <div className="frame-child" />
+          </div>
+          <div className="search-parent">
+            <div className="search">Search</div>
+            <img className="search-1-icon" alt="" src="/search-1.svg" />
+          </div>
+          <div className="chevron-down-1-group">
+            <img
+              className="chevron-down-1-icon1"
+              alt=""
+              src="/chevrondown-1.svg"
+            />
+            <div className="last-30-days">Last 30 days</div>
+          </div>
+        </div>
+        <div className="search1">
+          <div className="search">Search</div>
+          <img className="search-1-icon" alt="" src="/search-1.svg" />
+        </div>
+        <div className="hello-krishna">Hello Krishna 👋🏼,</div>
+        <div className="earning">
+          <img className="earning-child" alt="" src="/rectangle-15.svg" />
+          <div className="orders-parent">
+            <div className="orders">Orders</div>
+            <div className="k">$2.4k</div>
+            <img className="frame-item" alt="" src="/group-103.svg" />
+            <div className="this-month-parent">
+              <div className="this-month">
+                <b>2%</b>
+                <span className="this-month1"> this month</span>
+              </div>
+              <img className="arrow-up-1-icon" alt="" src="/arrowup-1.svg" />
+            </div>
+          </div>
+          <div className="frame-group">
+            <div className="earning-parent">
+              <div className="earning1">Earning</div>
+              <div className="k1">$198k</div>
+              <img className="frame-item" alt="" src="/group-101.svg" />
+              <div className="this-month-group">
+                <div className="this-month2">
+                  <b>37.8%</b>
+                  <span className="this-month3"> this month</span>
+                </div>
+                <img className="arrow-up-1-icon" alt="" src="/arrowup-11.svg" />
+              </div>
+            </div>
+            <div className="balance-parent">
+              <div className="balance">Balance</div>
+              <div className="k">$2.4k</div>
+              <img className="frame-item" alt="" src="/group-102.svg" />
+              <div className="this-month-parent">
+                <div className="this-month">
+                  <b>2%</b>
+                  <span className="this-month1"> this month</span>
+                </div>
+                <img className="arrow-up-1-icon" alt="" src="/arrowup-12.svg" />
+              </div>
+            </div>
+            <div className="total-sales-parent">
+              <div className="total-sales1">Total Sales</div>
+              <div className="k3">$89k</div>
+              <img className="frame-item" alt="" src="/group-103.svg" />
+              <div className="this-week-parent">
+                <div className="this-week">
+                  <b>11%</b>
+                  <span className="this-month1"> this week</span>
+                </div>
+                <img className="arrow-up-1-icon" alt="" src="/arrowup-13.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="side-menu">
+        <div className="frame1">
+          <div className="dashboard-parent">
+            <div className="dashboard">Dashboard</div>
+            <img className="setting-1-icon" alt="" src="/setting-1.svg" />
+          </div>
+          <div className="frame2">
+            <div className="list-menu">
+              <img
+                className="icon-24-outline-key-squa"
+                alt=""
+                src="/icon--24--outline--keysquare.svg"
+              />
+              <div className="dashboard1">Dashboard</div>
+            </div>
+          </div>
+        </div>
+        <div className="frame3">
+          <div className="frame4">
+            <div className="frame5">
+              <div className="list-menu1">
+                <img
+                  className="icon-24-outline-key-squa"
+                  alt=""
+                  src="/3dsquare-1.svg"
+                />
+                <div className="income">Product</div>
+                <img
+                  className="chevron-right-2-icon"
+                  alt=""
+                  src="/chevronright-2.svg"
+                />
+              </div>
+            </div>
+            <div className="frame6">
+              <div className="list-menu1">
+                <img
+                  className="icon-24-outline-key-squa"
+                  alt=""
+                  src="/usersquare-1.svg"
+                />
+                <div className="income">Customers</div>
+                <img
+                  className="chevron-right-2-icon"
+                  alt=""
+                  src="/chevronright-2.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="frame7">
+            <div className="frame5">
+              <div className="list-menu1">
+                <img
+                  className="icon-24-outline-key-squa"
+                  alt=""
+                  src="/walletmoney-2.svg"
+                />
+                <div className="income">Income</div>
+                <img
+                  className="chevron-right-2-icon"
+                  alt=""
+                  src="/chevronright-2.svg"
+                />
+              </div>
+            </div>
+            <div className="frame6">
+              <div className="list-menu1">
+                <img
+                  className="icon-24-outline-key-squa"
+                  alt=""
+                  src="/discountshape-1.svg"
+                />
+                <div className="income">Promote</div>
+                <img
+                  className="chevron-right-2-icon"
+                  alt=""
+                  src="/chevronright-2.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="frame10">
+            <div className="frame11">
+              <img className="ellipse-icon" alt="" src="/ellipse-8@2x.png" />
+              <div className="krishna-parent">
+                <div className="krishna">Krishna</div>
+                <div className="software-engineer">Software Engineer</div>
+              </div>
+              <img
+                className="chevron-down-2-icon"
+                alt=""
+                src="/chevrondown-2.svg"
+              />
+            </div>
+            <div className="frame5">
+              <div className="list-menu1">
+                <img
+                  className="icon-24-outline-key-squa"
+                  alt=""
+                  src="/messagequestion-1.svg"
+                />
+                <div className="income">Help</div>
+                <img
+                  className="chevron-right-2-icon"
+                  alt=""
+                  src="/chevronright-2.svg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDashboard;
